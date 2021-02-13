d23848bdec5f5f66c16df3fe4cd31f5b3d4027ff
thyago.cormick@gmail.com <thyago.cormick@gmail.com>
2021-02-13 20:44:52 -0300

    test

--- src/components/create/character/Character.vue
+++ src/components/create/character/Character.vue
@@ -2,46 +2,62 @@
     <div>
         <h1>New Character</h1>
 
-        <el-form ref="form" :model="heroine" label-width="15%" class="form-center">
-            <el-form-item label="Name">
-                <el-input v-model="heroine.name"></el-input>
+        <el-form ref="form" :model="character" label-width="15%" class="form-center mt-4">
+            <el-row :gutter="30">
+                <el-col :span="12" >
+                    <div>
+                        <el-input :span="8" placeholder="Name" v-model="input1" id="name">
+                            <label for="name" slot="prepend">Name</label>
+                        </el-input>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div>
+                        <el-input :span="8" placeholder="Name" v-model="input1" id="name">
+                            <label for="name" slot="prepend">Name</label>
+                        </el-input>
+                    </div>
+                </el-col>
+            </el-row>
+
+
+            <!-- 
+            <el-form-item label="Names" for="names">
+                <el-input id="names" v-model="character.name"></el-input>
             </el-form-item>
-            <el-form-item label="Heroine Tier" >
-                <el-select v-model="heroine.region" placeholder="Please Select The Tier" style="width: 100%">
-                <el-option label="T1" value="t1"></el-option>
-                <el-option label="T2" value="t2"></el-option>
-                <el-option label="T3" value="t3"></el-option>
-                <el-option label="T4" value="t4"></el-option>
-                </el-select>
-            </el-form-item>
+            <el-radio-group v-model="radio">
+                <el-radio :label="3">Option A</el-radio>
+                <el-radio :label="6">Option B</el-radio>
+                <el-radio :label="9">Option C</el-radio>
+            </el-radio-group>
 
             <el-form-item label="Power">
                 <el-col :span="8">
-                    <el-input-number v-model="heroine.power" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                    <el-input-number v-model="character.power" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
                 </el-col>
                 <el-col class="line" :span="8">Speed</el-col>
                 <el-col :span="8">
-                    <el-input-number v-model="heroine.speed" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                    <el-input-number v-model="character.speed" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
                 </el-col>
             </el-form-item>
             <el-form-item label="Skill">
                 <el-col :span="8">
-                    <el-input-number v-model="heroine.skill" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                    <el-input-number v-model="character.skill" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
                 </el-col>
                 <el-col class="line" :span="8">Mental</el-col>
                 <el-col :span="8">
-                    <el-input-number v-model="heroine.mental" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                    <el-input-number v-model="character.mental" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
                 </el-col>
             </el-form-item>
             <el-form-item label="Expereince">
                 <el-col :span="8">
-                    <el-input-number v-model="heroine.experience" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                    <el-input-number v-model="character.experience" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
                 </el-col>
             </el-form-item>
 
             <hr>
             <el-form-item label="Buffs">
-                <el-checkbox-group v-model="heroine.buff">
+                <el-checkbox-group v-model="character.buff">
                 <el-checkbox label="Fire Expert" name="buff"></el-checkbox>
                 <el-checkbox label="Air Expert" name="buff"></el-checkbox>
                 <el-checkbox label="Earth Expert" name="buff"></el-checkbox>
@@ -53,7 +69,7 @@
             </el-form-item>
             <hr>
             <el-form-item label="De-Buffs">
-                <el-checkbox-group v-model="heroine.debuff">
+                <el-checkbox-group v-model="character.debuff">
                 <el-checkbox label="Fire Inexperienced" name="debuff"></el-checkbox>
                 <el-checkbox label="Air Inexperienced" name="debuff"></el-checkbox>
                 <el-checkbox label="Earth Inexperienced" name="debuff"></el-checkbox>
@@ -67,12 +83,12 @@
             <hr>
             <el-form-item label="URL">
                 <el-col :span="8">
-                    <el-input v-model="heroine.url"></el-input>
+                    <el-input v-model="character.url"></el-input>
                 </el-col>
                     <div class="demo-image__preview">
                         <el-image 
                             style="width: 15%"
-                            :src="heroine.url">
+                            :src="character.url">
                         </el-image>
                     </div>   
             </el-form-item>
@@ -82,34 +98,46 @@
             <el-form-item>
                 <el-button type="primary" @click="onSubmit">Create</el-button>
                 <el-button>Cancel</el-button>
-            </el-form-item>
+            </el-form-item> -->
             </el-form>
 </div>
 </template>
 
 <script>
-  export default {
+    export default {
     data() {
-      return {
-        heroine: {
-          name: '',
-          power: 1,
-          speed: 1,
-          skill: 1,
-          mental: 1,
-          experience: 1,
-          buff: [],
-          debuff: [],
-          resource: '',
-          desc: '',
-          url: ''
+        return {
+            character: {
+                name: '',
+                power: 1,
+                speed: 1,
+                energy: 1,
+                spirit: 1,
+                xp: 1,
+                hp: 100,
+                mana: 100,
+                bio: '',
+                url: ''
+            },
+            heroine: {
+                name: '',
+                power: 1,
+                speed: 1,
+                skill: 1,
+                mental: 1,
+                experience: 1,
+                buff: [],
+                debuff: [],
+                resource: '',
+                desc: '',
+                url: ''
+            },
         }
-      }
     },
     methods: {
-      onSubmit() {
+        onSubmit() {
         console.log(this.heroine);
-      }
+        }
     }
-  }
+    }
 </script>

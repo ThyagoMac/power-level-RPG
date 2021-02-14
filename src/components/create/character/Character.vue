4fe15427f8d3aceed3d756219b8214c377d86987
thyago.cormick@gmail.com <thyago.cormick@gmail.com>
2021-02-14 13:43:51 -0300

    [upd] char create

--- src/components/create/character/Character.vue
+++ src/components/create/character/Character.vue
@@ -2,159 +2,176 @@
     <div>
         <h1>New Character</h1>
 
-        <el-form ref="form" :model="character" label-width="15%" class="form-center mt-4">
-            <el-row :gutter="30">
-                <el-col :sm="24" :md="12">
-                    <div>
-                        <el-input :span="8" placeholder="Name" v-model="character.name" id="name">
-                            <label for="name" slot="prepend">Name1</label>
-                        </el-input>
+        <el-form ref="form" :model="race" label-width="15%" class="form-center mt-4">
+            <div class="image__preview">
+                <el-image 
+                    style="width: 100px; height: 100px"
+                    :src="race.url"
+                    :preview-src-list="race.urlArray"
+                    fit="cover"
+                    lazy
+                >
+                    <div slot="placeholder" class="image-slot">
+                        Loading<span class="dot">...</span>
                     </div>
-                </el-col>
+                    <div slot="error" class="image-slot">
+                        <i class="el-icon-picture-outline"></i>
+                    </div>
+                </el-image>
+            </div>
+            <el-row :gutter="30" class="my-1">
                 <el-col :sm="24" :md="12">
                     <div>
-                        <el-input :span="8" placeholder="Name" v-model="character.name" id="name">
+                        <el-input placeholder="Name" v-model="race.name" id="name">
                             <label for="name" slot="prepend">Name</label>
                         </el-input>
                     </div>
                 </el-col>
+                <el-col :sm="24" :md="12">
+                    <div>
+                        <el-input placeholder="https://" v-model.lazy="race.url" @change="reloadUrl" id="url">
+                            <label for="url" slot="prepend">Url</label>
+                        </el-input>
+                    </div>
+                </el-col>
             </el-row>
-            name: '',
-                power: 1,
-                speed: 1,
-                energy: 1,
-                spirit: 1,
-                xp: 1,
-                hp: 100,
-                mana: 100,
-                bio: '',
-                url: '',
-                aura: false,
-                transform: 0,
-                haveKaioken: false,
-                kaioken: 0
-                <!-- 
-            <el-form-item label="Names" for="names">
-                <el-input id="names" v-model="character.name"></el-input>
-            </el-form-item>
-            <el-radio-group v-model="radio">
-                <el-radio :label="3">Option A</el-radio>
-                <el-radio :label="6">Option B</el-radio>
-                <el-radio :label="9">Option C</el-radio>
-            </el-radio-group>
 
-            <el-form-item label="Power">
-                <el-col :span="8">
-                    <el-input-number v-model="character.power" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+            <el-row :gutter="30" class="my-3">
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" type="success" size="medium">HP</el-tag>
+                        <el-input-number size="mini" v-model="race.hp" :min="-25" :max="25" :step="5" id="hp" :disabled=" totalPoints < 1 ? true : false" />
+                    </div>
                 </el-col>
-                <el-col class="line" :span="8">Speed</el-col>
-                <el-col :span="8">
-                    <el-input-number v-model="character.speed" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" size="medium">Mana</el-tag>
+                        <el-input-number size="mini" v-model="race.mana" :min="-25" :max="25" :step="5" id="mana" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
                 </el-col>
-            </el-form-item>
-            <el-form-item label="Skill">
-                <el-col :span="8">
-                    <el-input-number v-model="character.skill" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" size="medium">Energy</el-tag>
+                        <el-input-number size="mini" v-model="race.energy" :min="0" :max="5" id="energy" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
                 </el-col>
-                <el-col class="line" :span="8">Mental</el-col>
-                <el-col :span="8">
-                    <el-input-number v-model="character.mental" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" size="medium">Spirit</el-tag>
+                        <el-input-number size="mini" v-model="race.spirit" :min="0" :max="5" id="spirit" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
                 </el-col>
-            </el-form-item>
-            <el-form-item label="Expereince">
-                <el-col :span="8">
-                    <el-input-number v-model="character.experience" controls-position="right" :min="1" :max="10" style="width: 100%"></el-input-number>
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" type="warning" size="medium">Speed</el-tag>
+                        <el-input-number size="mini" v-model="race.speed" :min="0" :max="5" id="speed" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
                 </el-col>
-            </el-form-item>
-
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" type="success" size="medium">Regeneration</el-tag>
+                        <el-input-number size="mini" v-model="race.regen" :min="0" :max="10" :step="10" id="regen" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
+                </el-col>
+            </el-row>
+            <h5> <a @click="race.speed = 0, race.energy = 0, race.spirit = 0, race.mana = 0, race.hp = 0, race.regen = 0"> Pontos Restantes:</a> {{ totalPoints }}</h5>
             <hr>
-            <el-form-item label="Buffs">
-                <el-checkbox-group v-model="character.buff">
-                <el-checkbox label="Fire Expert" name="buff"></el-checkbox>
-                <el-checkbox label="Air Expert" name="buff"></el-checkbox>
-                <el-checkbox label="Earth Expert" name="buff"></el-checkbox>
-                <el-checkbox label="Water Expert" name="buff"></el-checkbox>
-                <el-checkbox label="Flyer" name="buff"></el-checkbox>
-                <el-checkbox label="Well Experienced" name="buff"></el-checkbox>
-                <el-checkbox label="Combatent" name="buff"></el-checkbox>
-                </el-checkbox-group>
-            </el-form-item>
+            <el-row :gutter="30" class="my-3">
+                <el-form-item label="BIO">
+                    <el-input type="textarea" v-model="race.bio"></el-input>
+                </el-form-item>
+            </el-row>
             <hr>
-            <el-form-item label="De-Buffs">
-                <el-checkbox-group v-model="character.debuff">
-                <el-checkbox label="Fire Inexperienced" name="debuff"></el-checkbox>
-                <el-checkbox label="Air Inexperienced" name="debuff"></el-checkbox>
-                <el-checkbox label="Earth Inexperienced" name="debuff"></el-checkbox>
-                <el-checkbox label="Water Inexperienced" name="debuff"></el-checkbox>
-                <el-checkbox label="Bad Experiences" name="debuff"></el-checkbox>
-                <el-checkbox label="Bad Combatent" name="debuff"></el-checkbox>
-                <el-checkbox label="Easily to Drain" name="debuff"></el-checkbox>
-                <el-checkbox label="Drainable" name="debuff"></el-checkbox>
-                </el-checkbox-group>
-            </el-form-item>
-            <hr>
-            <el-form-item label="URL">
-                <el-col :span="8">
-                    <el-input v-model="character.url"></el-input>
-                </el-col>
-                    <div class="demo-image__preview">
-                        <el-image 
-                            style="width: 15%"
-                            :src="character.url">
-                        </el-image>
-                    </div>   
-            </el-form-item>
-            <el-form-item label="BIO">
-                <el-input type="textarea" v-model="heroine.desc"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">Create</el-button>
-                <el-button>Cancel</el-button>
-            </el-form-item>-->
-            </el-form> 
+            <el-row :gutter="30" class="my-3">
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">Finish</el-button>
+                    <el-button 
+                        @click="race=newRace">Reset</el-button>
+                </el-form-item>
+            </el-row>
+        </el-form>
 </div>
 </template>
 
 <script>
     export default {
-    data() {
-        return {
-            character: {
-                name: '',
-                power: 1,
-                speed: 1,
-                energy: 1,
-                spirit: 1,
-                mana: 0,
-                hp: 100,
-                class: '',
-                race_id: '',
-                aura: false,
-                transform: 0,
-                have_rage: false,
-                rage: 0,
-                bio: '',
-                url: '',
+        data() {
+            return {
+                race: {
+                    name: '',
+                    regen: 0,
+                    speed: 0,
+                    energy: 0,
+                    spirit: 0,
+                    mana: 0,
+                    hp: 0,
+                    bio: '',
+                    url: '',
+                    urlArray: [''],
+                },
+                newRace: {
+                    name: '',
+                    speed: 0,
+                    energy: 0,
+                    spirit: 0,
+                    mana: 0,
+                    hp: 0,
+                    bio: '',
+                    url: '',
+                    urlArray: [],
+                },
+                character: {
+                    name: '',
+                    power: 1,
+                    regen: 0,
+                    speed: 1,
+                    energy: 1,
+                    spirit: 1,
+                    mana: 0,
+                    hp: 100,
+                    class: '',
+                    race_id: '',
+                    aura: false,
+                    transform: 0,
+                    have_rage: false,
+                    rage: 0,
+                    bio: '',
+                    url: '',
+                },
+                races: [],
+                left_points: 10,
+            }
+        },
+        methods: {
+            onSubmit () {
+                if (localStorage.getItem('races')) {
+                    let upd = JSON.parse(localStorage.getItem('races'))
+                    upd.push(this.race)
+                    localStorage.setItem('races', JSON.stringify(upd));
+
+                    this.race = this.newRace;
+                } else {
+                    this.races.push(this.race);
+                    localStorage.setItem('races', JSON.stringify(this.races));
+                    this.race = this.newRace;
+                }
+                console.log(localStorage.getItem('races'));
             },
-            heroine: {
-                name: '',
-                power: 1,
-                speed: 1,
-                skill: 1,
-                mental: 1,
-                experience: 1,
-                buff: [],
-                debuff: [],
-                resource: '',
-                desc: '',
-                url: ''
-            },
-        }
-    },
-    methods: {
-        onSubmit() {
-        console.log(this.character);
-        }
-    }
+            reloadUrl () {
+                this.race.urlArray = [];
+                this.race.urlArray.push(this.race.url);
+            }
+        },
+        computed: {
+            totalPoints () {
+                return (this.left_points - (this.race.speed + this.race.energy + this.race.spirit)) - ((this.race.hp/5) + (this.race.mana/5));                
+            }
+        },  
     }
 </script>
+
+<style scoped>
+    .tag-label {
+        width: 130px;
+    }
+</style>
--- src/components/create/race/Race.vue
+++ src/components/create/race/Race.vue
@@ -37,13 +37,13 @@
             </el-row>
 
             <el-row :gutter="30" class="my-3">
-                <el-col :sm="24" :md="5" class="my-1">
+                <el-col :sm="24" :md="4" class="my-1">
                     <div>
                         <el-tag class="tag-label" type="success" size="medium">HP</el-tag>
                         <el-input-number size="mini" v-model="race.hp" :min="-25" :max="25" :step="5" id="hp" :disabled=" totalPoints < 1 ? true : false" />
                     </div>
                 </el-col>
-                <el-col :sm="24" :md="5" class="my-1">
+                <el-col :sm="24" :md="4" class="my-1">
                     <div>
                         <el-tag class="tag-label" size="medium">Mana</el-tag>
                         <el-input-number size="mini" v-model="race.mana" :min="-25" :max="25" :step="5" id="mana" :disabled=" totalPoints < 1 ? true : false"/>
@@ -55,20 +55,26 @@
                         <el-input-number size="mini" v-model="race.energy" :min="0" :max="5" id="energy" :disabled=" totalPoints < 1 ? true : false"/>
                     </div>
                 </el-col>
-                <el-col :sm="24" :md="5" class="my-1">
+                <el-col :sm="24" :md="4" class="my-1">
                     <div>
                         <el-tag class="tag-label" size="medium">Spirit</el-tag>
                         <el-input-number size="mini" v-model="race.spirit" :min="0" :max="5" id="spirit" :disabled=" totalPoints < 1 ? true : false"/>
                     </div>
                 </el-col>
-                <el-col :sm="24" :md="5" class="my-1">
+                <el-col :sm="24" :md="4" class="my-1">
                     <div>
                         <el-tag class="tag-label" type="warning" size="medium">Speed</el-tag>
                         <el-input-number size="mini" v-model="race.speed" :min="0" :max="5" id="speed" :disabled=" totalPoints < 1 ? true : false"/>
                     </div>
                 </el-col>
+                <el-col :sm="24" :md="4" class="my-1">
+                    <div>
+                        <el-tag class="tag-label" type="success" size="medium">Regeneration</el-tag>
+                        <el-input-number size="mini" v-model="race.regen" :min="0" :max="10" :step="10" id="regen" :disabled=" totalPoints < 1 ? true : false"/>
+                    </div>
+                </el-col>
             </el-row>
-            <h5> <a @click="race.speed = 0, race.energy = 0, race.spirit = 0, race.mana = 0, race.hp = 0"> Pontos Restantes:</a> {{ totalPoints }}</h5>
+            <h5> <a @click="race.speed = 0, race.energy = 0, race.spirit = 0, race.mana = 0, race.hp = 0, race.regen = 0"> Pontos Restantes:</a> {{ totalPoints }}</h5>
             <hr>
             <el-row :gutter="30" class="my-3">
                 <el-form-item label="BIO">
@@ -93,6 +99,7 @@
             return {
                 race: {
                     name: '',
+                    regen: 0,
                     speed: 0,
                     energy: 0,
                     spirit: 0,
@@ -104,6 +111,7 @@
                 },
                 newRace: {
                     name: '',
+                    regen: 0,
                     speed: 0,
                     energy: 0,
                     spirit: 0,
@@ -139,7 +147,7 @@
         },
         computed: {
             totalPoints () {
-                return (this.left_points - (this.race.speed + this.race.energy + this.race.spirit)) - ((this.race.hp/5) + (this.race.mana/5));                
+                return (this.left_points - (this.race.speed + this.race.energy + this.race.spirit)) - ((this.race.hp/5) + (this.race.mana/5)) - (this.race.regen);                
             }
         },  
     }

bf207ee78476dbe0fc99d275745c65bf3c5fadf9
thyago.cormick@gmail.com <thyago.cormick@gmail.com>
2021-02-28 17:20:29 -0300

    [UPD] trash deleted

--- src/components/view/Index.vue
+++ src/components/view/Index.vue
@@ -3,19 +3,19 @@
     <el-tabs type="border-card" tab-position="">
     <el-tab-pane>
         <span slot="label"><i class="el-icon-document"></i> Chars</span>
-        <characters 
+        <Table 
             :itens="characters"
         />
     </el-tab-pane>
     <el-tab-pane>
         <span slot="label"><i class="el-icon-s-custom"></i> Races</span>
-        <characters 
+        <Table 
             :itens="races"
         />
     </el-tab-pane>
     <el-tab-pane>
         <span slot="label"><i class="el-icon-s-cooperation"></i> Classes</span>
-        <characters 
+        <Table 
             :itens="classes"
         />
     </el-tab-pane>
@@ -25,12 +25,12 @@
 </template>
 
 <script>
-    import Characters from './characters/Characters'
+    import Table from '../shared/Table'
     /* import Classes from './classes/Classes'
     import Races from './races/Races' */
 
     export default {
-        components: { Characters, /* Classes, Races */ },
+        components: { Table, /* Characters,  Classes, Races */ },
         data() {
             return {
                 characters: [],
